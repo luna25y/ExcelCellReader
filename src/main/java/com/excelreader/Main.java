01e24b261228dab4d55f81fc885366d8bfb80765
luna <yyrrjj25@gmail.com>
2025-03-04 16:36:11 -0500

    Update

--- src/main/java/com/excelreader/Main.java
+++ src/main/java/com/excelreader/Main.java
@@ -1,9 +1,15 @@
 package com.excelreader;
 
 import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
-        Application.launch(ExcelReaderApp.class, args);
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        new ExcelReaderApp().start(stage);
     }
 } 
